e831dd46a51254a9b8f866c0b1f28f29eff7ea37
JenriqueGuerrero <jenriqueguerrero@mail.uniatlantico.edu.co>
2025-02-04 11:03:57 -0500

    create function

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const friend = [];
+
+function addFriend() {
+    let getFriend = document.getElementById('nameFriend').value.trim();
+
+    if (getFriend !== "") {
+        friend.push(getFriend);
+        updateFriendList();
+    } else {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+
+    clearInput();
+}
+
+function randomizeFriend() {
+    if (friend.length > 2) { 
+        let randomIndex = Math.floor(Math.random() * friend.length);
+        let selectedFriend = friend[randomIndex];
+        document.getElementById('result').innerHTML = `El amigo seleccionado es: <strong>${selectedFriend}</strong>`;
+    } else {
+        alert("Debe ingresar más de dos nombres para realizar el sorteo.");
+    }
+}
+
+function updateFriendList() {
+    let friendList = document.getElementById('friendList');
+    friendList.innerHTML = "";
+
+    friend.forEach(name => {
+        let listItem = document.createElement('li');
+        listItem.textContent = name;
+        friendList.appendChild(listItem);
+    });
+}
+
+function clearInput() {
+    document.getElementById('nameFriend').value = '';
+}
